42dfde2ae9a9c917019638c8b0ddc2b14f4a1796
noymak <petchtot@gmail.com>
2022-07-09 20:55:13 +0700

    ฟายเบด

--- project-main/lib/main.dart
+++ project-main/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ForgotPass(),
+      home: WelcomeScreen(),
       routes: {
         '/register': ((context) => RegisterScreen()),
         '/route': ((context) => RouteScreen()),
