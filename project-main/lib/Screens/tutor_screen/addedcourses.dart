8ded2c21f809d5fbdf692ff09b7f2957ff313c08
USERPUE-B709TN6\User <petchtot@gmail.com>
2022-08-24 20:59:35 +0700

    เพิ่มหน้า

--- project-main/lib/Screens/tutor_screen/addedcourses.dart
+++ project-main/lib/Screens/tutor_screen/addedcourses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:luk_to_learn/constants.dart';
 
 class AddedCourses extends StatefulWidget {
@@ -20,17 +21,36 @@ class _AddedCoursesState extends State<AddedCourses> {
               height: 200,
               width: size.width,
               color: kPrimaryColors,
+              child: CircleAvatar(
+                child: Icon(Icons.add_a_photo_outlined,size: 60,),
+              ),
             ),
             SizedBox(
               height: 10,
             ),
-            Container(
-              width: 90,
-              height: 90,
-              child: CircleAvatar(
-                radius: 15,
-                backgroundImage: AssetImage('assets/images/profile.jpg'),
-              ),
+            Stack(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: CircleAvatar(
+                      radius: 55,
+                      backgroundImage: AssetImage('assets/images/profile.jpg')),
+                ),
+                Positioned(
+                  right: 3,
+                  bottom: 3,
+                  child: Container(
+                    width: 35,
+                    height: 35,
+                    child: CircleAvatar(
+                      child: Icon(Icons.add_a_photo_outlined),
+                      backgroundColor: Colors.white,
+                      radius: 55,
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 10,
@@ -38,39 +58,99 @@ class _AddedCoursesState extends State<AddedCourses> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('อิอิ'),
-                Container(width: 300, child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                Text(
+                  'ชื่อผู้สอน',
+                  style: GoogleFonts.kanit(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  width: 300,
+                  height: 40,
+                  child: TextField(
+                    
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
                   ),
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.text,
-                ),),
-                Text('อิอิ'),
-                Container(width: 300, child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'ชื่อคอร์ส',
+                  style: GoogleFonts.kanit(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  width: 300,
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
                   ),
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.text,
-                ),),
-                Text('อิอิ'),
-                Container(width: 300, child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'ราคา',
+                  style: GoogleFonts.kanit(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  width: 300,
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.number,
                   ),
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.text,
-                ),),
-                Text('อิอิ'),
-                Container(width: 300, child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'รายละเอียดคอร์ส',
+                  style: GoogleFonts.kanit(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  width: 300,
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
                   ),
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.text,
-                ),),
+                ),
               ],
+            ),
+            SizedBox(height: 15),
+            GestureDetector(
+              onTap: () {
+                          Navigator.pushNamed(context, '/checkinfocourse');
+                        },
+              child: Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    color: kPrimaryColor),
+                    child: Center(
+                      child: Text('เพิ่มคอร์ส',
+                      style: GoogleFonts.kanit(
+                          fontSize: 20, fontWeight: FontWeight.bold,color: kPrimaryLightColor),),
+                    ),
+              ),
             ),
           ],
         ),
